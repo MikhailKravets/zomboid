e4153f2406811c55770b32125d4c941707cf5a2d
mikekravets <michkravets@gmail.com>
2024-10-04 11:00:05 +0300

    Write stream method for Zomboid wrapper

--- zombo/src/lib.rs
+++ zombo/src/lib.rs
@@ -1,16 +1,80 @@
-use std::path::Path;
+use model::Item;
+use std::fmt::Display;
 
 pub mod model;
 
 #[derive(Debug)]
-struct Zomboid {
-    path: Path,
+pub struct Zomboid<T> {
+    it: T,
+}
+
+#[derive(Debug)]
+pub struct Table {
+    header: Option<Vec<&'static str>>,
+    data: Vec<Item>,
+}
+
+impl<T, E> Zomboid<T>
+where
+    T: Iterator<Item = Result<Item, E>>,
+    E: std::error::Error,
+{
+    pub fn new(it: T) -> Self {
+        Self { it }
+    }
+
+    pub fn stream(&mut self, take: usize, skip: usize) -> Result<Table, E> {
+        let it = &mut self.it;
+        let items: Result<Vec<Item>, E> = it.skip(skip).take(take).collect();
+
+        Ok(Table::new(items?).with_header(vec!["ID", "NAME", "TYPE", "CONDITION", "AMOUNT"]))
+    }
+}
+
+impl Table {
+    pub fn new(data: Vec<Item>) -> Self {
+        Self { header: None, data }
+    }
+
+    pub fn with_header(mut self, header: Vec<&'static str>) -> Self {
+        self.header = Some(header);
+        self
+    }
+}
+
+impl Display for Table {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let top = format!("┌{:─^114}┐", "");
+        let bot = format!("└{:─^114}┘", "");
+        let mid = format!("├{:─^114}┤", "");
+
+        writeln!(f, "{}", top)?;
+        if let Some(header) = &self.header {
+            for v in header {
+                write!(f, "│ {:^20} ", v)?;
+            }
+
+            write!(f, "│")?;
+            writeln!(f)?;
+
+            writeln!(f, "{}", mid)?;
+        }
+
+        for item in &self.data {
+            writeln!(
+                f,
+                "│ {:^20} │ {:^20} │ {:^20} │ {:^20} │ {:^20} │",
+                item.id, item.name, item.item_type, item.condition, item.amount
+            )?;
+        }
+        writeln!(f, "{}", bot)?;
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::model::Item;
-    use csv;
+    use crate::{model::Item, Zomboid};
     use std::{env::current_dir, path::PathBuf};
 
     fn data_path() -> PathBuf {
@@ -26,5 +90,12 @@ mod tests {
             let rec: Item = res.unwrap();
             println!("{:?}", rec);
         }
+    }
+
+    #[test]
+    fn zomboid_csv() {
+        let mut r = csv::Reader::from_path(data_path()).unwrap();
+        let mut z = Zomboid::new(r.deserialize());
+        println!("{}", z.stream(10, 0).unwrap())
     }
 }
